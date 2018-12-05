5b28b2f8a409ddbf229ad6a957a8032a516e5630
gebeto <slavik.nychkalo@gmail.com>
2018-12-05 09:11:15 +0200

    improve building configuration

--- computer-graphics/configs/webpack.serve.js
+++ computer-graphics/configs/webpack.serve.js
@@ -1,11 +1,11 @@
 const webpackMerge = require('webpack-merge');
 const development = require('./webpack.development');
 
-module.exports = (env) => webpackMerge(
-	development(env),
+module.exports = (env, opts) => webpackMerge(
+	development(env, opts),
 	{
 		devServer: {
-			port: 5000,
+			port: 5000 + opts.serveIndex,
 			host: '0.0.0.0'
 		},
 	}
--- computer-graphics/webpack.config.js
+++ computer-graphics/webpack.config.js
@@ -5,55 +5,59 @@ const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
-const envConfig = (env, opts) => {
-	return require(`./configs/webpack.${env}.js`)(env, {
-		path: __dirname,
-	});
-}
+const envConfig = (env, opts) => require(`./configs/webpack.${env}.js`)(env, opts);
 
-const baseConfig = (env, opts, ...args) => {
-	const projectPath = opts._.length ? path.resolve(__dirname, opts._[0]) : false;
-	if (!projectPath) throw "Project is not exists!";
-	const packagePath = path.resolve(projectPath, 'package.json');
-	if (!fs.existsSync(packagePath)) throw "Package.json are not found in project folder!";
+const baseConfig = (env, opts) => ({
+	entry: path.resolve(opts.path, 'src/index.tsx'),
 
-	return {
-		entry: path.resolve(projectPath, 'src/index.tsx'),
+	output: {
+		path: path.resolve(opts.path, 'dist'),
+		filename: 'bundle.js',
+	},
 
-		output: {
-			path: path.resolve(projectPath, 'dist'),
-			filename: 'bundle.js',
-		},
-
-		module: {
-			rules: [
-				{
-					test: /\.scss?/,
-					use: [
-						'style-loader',
-						'css-loader',
-						'sass-loader',
-					],
-				},
-				{ test: /\.tsx?/, loader: 'awesome-typescript-loader', },
-				{ test: /\.pug$/, loader: 'pug-loader', },
-				{ test: /\.hbs$/, loader: 'handlebars-loader' },
-			]
-		},
-
-		plugins: [	
-			new HtmlWebpackPlugin({
-				filename: 'index.html',
-				template: path.resolve(__dirname, 'templates/index.hbs'),
-				inject: false,
-				templateParameters: {
-					title: require(packagePath).name,
-				}
-			}),
+	module: {
+		rules: [
+			{
+				test: /\.scss?/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{ test: /\.tsx?/, loader: 'awesome-typescript-loader', },
+			{ test: /\.pug$/, loader: 'pug-loader', },
+			{ test: /\.hbs$/, loader: 'handlebars-loader' },
 		]
-	};
+	},
+
+	plugins: [	
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: path.resolve(__dirname, 'templates/index.hbs'),
+			inject: false,
+			templateParameters: {
+				title: require(path.resolve(opts.path, "package.json")).name,
+			}
+		}),
+	]
+});
+
+
+module.exports = (env, opts) => {
+	console.log(opts._);
+
+	return opts._.map((projectName, index) => {
+		const projectPath = path.resolve(__dirname, projectName);
+		// if (!projectPath) throw `Project is not exists!`;
+		const packagePath = path.resolve(projectPath, 'package.json');
+		if (!fs.existsSync(packagePath)) throw `Package.json are not found in project '${projectName}'!`;
+		
+		const opts = {
+			path: projectPath,
+			serveIndex: index,
+		};
+		return webpackMerge(baseConfig(env, opts), envConfig(env, opts));
+	});
 };
-
-
-module.exports = (env, opts) => webpackMerge(baseConfig(env, opts), envConfig(env, opts));
 
