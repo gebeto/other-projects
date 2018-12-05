22d07964dea093497b2818975c0b5f05a7e820d9
gebeto <slavik.nychkalo@gmail.com>
2018-12-05 08:35:06 +0200

    update config

--- computer-graphics/webpack.config.js
+++ computer-graphics/webpack.config.js
@@ -10,41 +10,43 @@ const envConfig = (env, opts) => {
 	});
 }
 
-const baseConfig = (env, opts) => ({
-	entry: path.resolve(opts.path, 'src/index.tsx'),
+const baseConfig = (env, opts) => {
+	return {
+		entry: path.resolve(opts.path, 'src/index.tsx'),
 
-	output: {
-		path: path.resolve(opts.path, 'dist'),
-		filename: 'bundle.js',
-	},
+		output: {
+			path: path.resolve(opts.path, 'dist'),
+			filename: 'bundle.js',
+		},
 
-	module: {
-		rules: [
-			{
-				test: /\.scss?/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader',
-				],
-			},
-			{ test: /\.tsx?/, loader: 'awesome-typescript-loader', },
-			{ test: /\.pug$/, loader: 'pug-loader', },
-			{ test: /\.hbs$/, loader: 'handlebars-loader' },
+		module: {
+			rules: [
+				{
+					test: /\.scss?/,
+					use: [
+						'style-loader',
+						'css-loader',
+						'sass-loader',
+					],
+				},
+				{ test: /\.tsx?/, loader: 'awesome-typescript-loader', },
+				{ test: /\.pug$/, loader: 'pug-loader', },
+				{ test: /\.hbs$/, loader: 'handlebars-loader' },
+			]
+		},
+
+		plugins: [	
+			new HtmlWebpackPlugin({
+				filename: 'index.html',
+				template: path.resolve(__dirname, 'templates/index.hbs'),
+				inject: false,
+				templateParameters: {
+					title: opts.package.name,
+				}
+			}),
 		]
-	},
-
-	plugins: [	
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: path.resolve(__dirname, 'templates/index.hbs'),
-			inject: false,
-			templateParameters: {
-				title: opts.package.name,
-			}
-		}),
-	]
-});
+	};
+};
 
 
 module.exports = (env, opts) => webpackMerge(baseConfig(env, opts), envConfig(env, opts));
