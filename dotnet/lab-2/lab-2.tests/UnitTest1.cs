cb0ded3584eaa51a11ee4324c61c013f92459779
gebeto <slavik.nychkalo@gmail.com>
2018-12-13 21:16:15 +0200

    update

--- dotnet/lab-2/lab-2.tests/UnitTest1.cs
+++ dotnet/lab-2/lab-2.tests/UnitTest1.cs
@@ -8,7 +8,12 @@ namespace lab_2.tests
         [Fact]
         public void Test1()
         {
-
+            Assert.Equal(0, 0);
+        }
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, 1);
         }
     }
 }
--- dotnet/lab-2/lab-2/Program.cs
+++ dotnet/lab-2/lab-2/Program.cs
@@ -6,28 +6,28 @@ namespace lab_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Selection(List<Animal> animals)
         {
-            List<Animal> animals = AnimalsGenerator.GenerateAnimals(100);
-
             // IEnumerable<Animal> lg50sm60 = animals.Where(a => a.age >= 50 && a.age <= 60).Select(x => x);
             IEnumerable<Animal> lg50sm60 = (
                 from animal in animals
                 where animal.age >= 50 && animal.age <= 60
+                orderby animal.age ascending
                 select animal
             );
-
-            Console.WriteLine("All");
-            foreach (Animal animal in animals)
-            {
-                Console.WriteLine("Animal: {0}, {1}, {2}", animal.name, animal.type, animal.age);
-            }
 
             Console.WriteLine("\nAnimals older then 50 years and smaller than 60:");
             foreach (Animal animal in lg50sm60)
             {
                 Console.WriteLine("Animal: {0}, {1}, {2}", animal.name, animal.type, animal.age);
             }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Animal> animals = AnimalsGenerator.GenerateAnimals(100);
+
+            Selection(animals);
             
         }
     }
